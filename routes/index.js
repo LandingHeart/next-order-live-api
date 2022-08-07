25e5730d0335dfac0970b8ad39220df60ceab6be
LandingHeart <shinan.liu@yahoo.com>
2022-08-07 00:56:34 -0400

    final fix

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const api = require("./api");
 
-router.use("/api", api);
+const employeeController = require("../controllers/employee.controller");
+const categoryController = require("../controllers/category.controller");
+const comboController = require("../controllers/combo.controller");
+const invoiceController = require("../controllers/invoice.controller");
+const menuController = require("../controllers/menu.controller");
+
+router.get("/api/employee", employeeController.findAll);
+router.post("/api/employee/create", employeeController.create);
+
+router.get("/api/category", categoryController.findAll);
+router.post("/api/category/create", categoryController.create);
+
+router.get("/api/combo", comboController.findAll);
+router.post("/api/combo/create", comboController.create);
+
+router.get("/api/invoice", invoiceController.findAll);
+router.post("/api/invoice/create", invoiceController.create);
+
+router.get("/api/menu", menuController.findAll);
+router.post("/api/menu/create", menuController.create);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "./configs/.env") });
 const index = require("./routes/index");
 const PORT = process.env.PORT || 8080;
-
+const db = require("./models");
 app.use(express.json());
 
 app.use("/", index);
@@ -13,14 +13,14 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-// db.sequelize
-//   .sync({ force: true })
-//   .then(() => {
-//     console.log("Synced and connected db.");
-//   })
-//   .catch((err) => {
-//     console.log("Failed to sync db: " + err.message);
-//   });
+db.sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log("Synced and connected db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
