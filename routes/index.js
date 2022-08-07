c7e847b07dde1f95f4e9d694981a0adb3ef815c0
LandingHeart <shinan.liu@yahoo.com>
2022-08-07 01:02:31 -0400

    save

--- routes/index.js
+++ routes/index.js
@@ -10,16 +10,16 @@ const menuController = require("../controllers/menu.controller");
 router.get("/api/employee", employeeController.findAll);
 router.post("/api/employee/create", employeeController.create);
 
-router.get("/api/category", categoryController.findAll);
-router.post("/api/category/create", categoryController.create);
+// router.get("/api/category", categoryController.findAll);
+// router.post("/api/category/create", categoryController.create);
 
-router.get("/api/combo", comboController.findAll);
-router.post("/api/combo/create", comboController.create);
+// router.get("/api/combo", comboController.findAll);
+// router.post("/api/combo/create", comboController.create);
 
-router.get("/api/invoice", invoiceController.findAll);
-router.post("/api/invoice/create", invoiceController.create);
+// router.get("/api/invoice", invoiceController.findAll);
+// router.post("/api/invoice/create", invoiceController.create);
 
-router.get("/api/menu", menuController.findAll);
-router.post("/api/menu/create", menuController.create);
+// router.get("/api/menu", menuController.findAll);
+// router.post("/api/menu/create", menuController.create);
 
 module.exports = router;
