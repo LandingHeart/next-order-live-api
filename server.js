bbda17c4adcc46a4f8272c97728039c5ea7d5425
LandingHeart <shinan.liu@yahoo.com>
2022-08-07 01:37:49 -0400

    last

--- server.js
+++ server.js
@@ -4,34 +4,13 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "./configs/.env") });
 const cors = require("cors");
 const PORT = process.env.PORT || 8080;
-const db = require("./models");
+
 app.use(express.json());
 
 app.use(cors());
-app.use(express.static(__dirname + "/public"));
 
 app.use(require("./routes"));
-
-// db.sequelize
-//   .sync({ force: true })
-//   .then(() => {
-//     console.log("Synced and connected db.");
-//   })
-//   .catch((err) => {
-//     console.log("Failed to sync db: " + err.message);
-//   });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-// connectPostgres();
-
-// async function connectPostgres() {
-//   try {
-//     await sequelize.authenticate();
-//     console.log("Connection has been established successfully.");
-//   } catch (error) {
-//     console.error("Unable to connect to the database:", error);
-//   }
-// }
